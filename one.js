dba5c17c83e3599a3c8d7137233a426efe2ce4e4
CoderAftab22 <155820039+coolsah@users.noreply.github.com>
2024-05-04 15:07:02 +0000

    this is my file

---
+++ one.js
@@ -0,0 +1,64 @@
+// let abc = "ruksar";
+// console.log(abc);
+// // console.log(anabiya)
+// console.log("hello");
+// console.log(typeof 3);
+// let d;
+// console.log(d);
+// abc = "aftab";
+// console.log(abc);
+// console.log(typeof abc);
+// const ab = 4;
+// let c = "aftab";
+// console.log(c);
+// console.log(ab);
+
+// var a = 10;
+// var a = 20;
+// console.log(c)
+// console.log(a)//global scope
+// {
+//     var c = 30;
+//     console.log(c)
+// console.log(a) //block scope
+// }
+// console.log(a)//global scope
+// console.log(c);
+// var v;
+// console.log(v);//hoisting
+// var v = "a";
+// let dbc = 5;
+// {
+//     let s = 5
+//     console.log(dbc)
+//     console.log(s)
+// }
+// console.log(dbc);
+// console.log(s)
+console.log("1"+2+3);//6 5 123
+console.log(1+2+"4");//7 3 34
+console.log(3+"4"+5+6); //3456
+console.log(2+3 +"4"+5+6)//5456
+console.log(true +"4");//true/frue 14
+console.log(1 + +"4");//14 5
+console.log(Number(true)+"4");//5
+ console.log(String(5)+4)
+ console.log(Boolean(1)+"4")//true4
+ console.log(3+ +"2"+Boolean("")+"2"+2)//5true22 5false22 //522
+ console.log(Boolean(true))
+ console.log(3+4+Boolean("str")+2+"2")//7str22 102 3+4+1+2+"2"102
+ const a = 10;
+ let b = a;
+//  console.log(b)//10
+   b = "ravi";
+   console.log(a)//10
+   console.log(b)//ravi
+   const c =[2,4,3];
+   let d = c;
+   d = [3]
+   console.log(d)
+   d = [...c,4];
+
+   console.log(c)//4 // 2,4,3,4
+   console.log(d)//234 2,4,3,4
+
