c9813871fad52aba01572cb6b0d1a4edbe802a70
CoderAftab22 <155820039+coolsah@users.noreply.github.com>
2024-05-09 11:26:53 +0000

    this is my date

---
+++ Code/date.js
@@ -0,0 +1,6 @@
+// date representt krta hai single momnet in time in a platform ka january 1970 se declare krta hai mili second mein hota hai ;
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString())
